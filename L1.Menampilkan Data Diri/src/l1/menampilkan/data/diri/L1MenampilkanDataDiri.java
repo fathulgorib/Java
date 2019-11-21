35267ba876c3a451ca45852af04e2428b3a864fc
fathulgorib <satuy.jeager10@gmail.com>
2019-11-20 14:32:48 -1200

    first commit

---
+++ L1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Fathul Gorib
+ * NIM   : A2.1900066
+ * Kelas : TI-1B
+ * 
+ * @author Satuyyy
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Fathul Gorib ");
+        System.out.println("ALAMAT\t\t         : Cimalaka ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 10 Februari 2001");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Bermain Game");
+        System.out.println("MAKES\t\t: Baso");
+        System.out.println("MIKES\t\t: Kopi Hitam");
+    }
+    
+}
